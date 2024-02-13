c0d2bd3888ac043c77eadc33fe49e96836b2b608
tunatuna123 <tuna200538@gmail.com>
2024-02-13 11:00:19 +0900

    Create

--- typescript/src/controllers/MarketController.ts
+++ typescript/src/controllers/MarketController.ts
@@ -22,4 +22,21 @@ class MarketController{
             return res.sendStatus(400);
         }
     }
+
+    // Create
+    createProduct = async (req: express.Request, res: express.Response) => {
+        try {
+            const {name, quantity, price, image} = req.body;
+            const product = new ProductModel({
+                name,
+                quantity,
+                price,
+                image
+            });
+            await product.save();
+            return res.status(200).json({message: "Created", data: product});
+        } catch (error) {
+            return res.status(400);
+        }
+    }
 }
