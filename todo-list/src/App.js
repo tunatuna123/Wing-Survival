fab75f43a16c55c9d66077d6807101b8f3be769a
tunatuna123 <tuna200538@gmail.com>
2024-02-16 16:58:06 +0900

    Design: App.css Btn update

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,29 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false)
   return (
-    <div className='App'>
+    <div className="App">
       <h1>My todos</h1>
-      <div className='todo-wrapper'>
-        <div className='todo-input'>
-          <div className='todo-input-item'>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
             <label>Title</label>
-            <input type='text' placeholder="What's the task title?"/>
+            <input type="text" placeholder="What's the task title?" />
           </div>
-          <div className='todo-input-item'>
+          <div className="todo-input-item">
             <label>Description</label>
-            <input type='text' placeholder="What's your description?"/>
+            <input type="text" placeholder="What's your description?" />
           </div>
-          <div className='todo-input-item'>
-            <button type='button' className='primaryBtn'>Add</button>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">
+              Add
+            </button>
           </div>
         </div>
-        <div className='btn-area'>
-          <button>Todo</button>
-          <button>Completed</button>
+        <div className="btn-area">
+          <button className={`isCompleteScreen ${isCompleteScreen==false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>
+            Todo
+            </button>
+          <button className={`isCompleteScreen ${isCompleteScreen==true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>
+            Completed
+            </button>
         </div>
-        <div className='todo-list'>
+        <div className="todo-list">
           <h1>Task 1</h1>
           <p>Description</p>
         </div>
